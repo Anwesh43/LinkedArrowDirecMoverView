a48efcd376d6170b2e882738644a3ce0b691d8ca
Anwesh43 <anweshthecool0@gmail.com>
2018-08-06 02:10:31 +0530

    adding LinkedLADM class

--- ladmview/src/main/java/com/anwesh/uiprojects/ladmview/LADMView.kt
+++ ladmview/src/main/java/com/anwesh/uiprojects/ladmview/LADMView.kt
@@ -145,4 +145,28 @@ class LADMView (ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedLADM(var i : Int) {
+
+        private var curr : LADMNode = LADMNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
